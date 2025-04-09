297b5845beb502f400c42b37fd0baae1b8c88861
Anvi Verma <Anvi.Verma@winjittechnologiespvtltd.local>
2025-04-09 10:50:04 +0530

    Task1 completed

---
+++ TaskDotNet/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using TaskDotNet.Models;
+
+namespace TaskDotNet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+         var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+
+        var result = new Response<WeatherForecast[]>(true, "Date fetched Successfully", forecast);
+        return Ok(result);
+    }
+}
---
+++ TaskDotNet/Models/Response.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskDotNet.Models
+{
+    public class Response<Data>
+    {
+        public bool isSuccess { get; set; }
+        public string? message { get; set; }
+        public Data? data { get; set; }
+
+        public Response(bool success,string Message,Data dataRecived)
+        {
+            isSuccess = success;
+            message = Message;
+            data = dataRecived;
+        }
+    }
+}
