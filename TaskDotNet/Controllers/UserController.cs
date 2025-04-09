57c6f99787676df5ac2210e6a38d265f5affa87f
Anvi Verma <Anvi.Verma@winjittechnologiespvtltd.local>
2025-04-09 16:36:26 +0530

    Task4 completed

---
+++ TaskDotNet/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskDotNet.Entities.IdentityEntities;
+
+namespace TaskDotNet.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser>? _userManager;
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet("get-users")]
+        [Authorize]
+        public async Task<IActionResult> GetUsers()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            var roles = await _userManager.GetRolesAsync(currentUser);
+
+            bool isAdmin = roles.Contains("Admin");
+
+            var users = _userManager.Users.ToList();
+
+            var result = new List<object>();
+
+            foreach (var user in users)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                if (isAdmin || userRoles.Contains("Employee"))
+                {
+                    result.Add(new
+                    {
+                        user.Id,
+                        user.UserName,
+                        user.Email,
+                        Roles = userRoles
+                    });
+                }
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
--- TaskDotNet/obj/Debug/net8.0/TaskDotNet.AssemblyInfo.cs
+++ TaskDotNet/obj/Debug/net8.0/TaskDotNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34d9cea6d353c778be20738d1f9d2cac13e6200")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4d98267fd356bed971e22272e384f0bb3c364e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
